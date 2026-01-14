a1ef6d7150c9f269ec61ceb7a6203b3992ab1026
pcakhilnadh <pcakhilnadh@gmail.com>
2026-01-15 00:03:46 +0530

    dockerized the application

--- server/index.ts
+++ server/index.ts
@@ -85,7 +85,8 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(port, "localhost", () => {
+  const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
+  httpServer.listen(port, host, () => {
     log(`serving on port ${port}`);
   });
 })();
